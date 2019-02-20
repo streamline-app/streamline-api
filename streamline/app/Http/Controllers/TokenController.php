1cd39b0565c6926314c3ea92479adc6267c5e4f0
Dan Morton <dmorton2297@gmail.com>
2019-02-20 16:57:18 -0500

    Tokens implemented in backend

---
+++ streamline/app/Http/Controllers/TokenController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class TokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DB::table('tokens')->get();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $token = new \App\Token;
+        $token -> userId = $request -> input('userId');
+        $token -> token = $request -> input('token');
+        $token -> save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function read($id)
+    {
+        $token = \App\Token::find($id);
+        if ($token == null) {
+            return response('', 404);
+        } else {
+            return $token;
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $token = \App\Token::find($id);
+
+        if ($token == null) {
+            return response('', 404);
+        } 
+
+        $token -> userId = $request -> input('userId');
+        $token -> token = $request -> input('token');
+        $token -> save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($token)
+    {
+        $token = \App\Token::where('token', $token) -> first();
+        $token -> delete();
+    }
+}
---
+++ streamline/database/migrations/2019_02_20_213007_token_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TokenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            // Unique ID of Task
+            $table->increments('id');
+            // Unique ID of owner
+            $table->integer('userId')->unsigned();
+            // token itself
+            $table->text('token');
+            $table->timestamps();
+        }); 
+
+        Schema::table('tokens', function (Blueprint $table) {
+            $table->foreign('userId')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
