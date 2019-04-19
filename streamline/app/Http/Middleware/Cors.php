4e01c695a888a4745903618cdd2a6b7ef01fc7c2
Scott Merritt <smerrit@purdue.edu>
2019-04-19 00:45:52 -0400

    feat -- added code to send Team files to a user upon request; updated CORS file to properly append necessary headers to different types of Responses

--- streamline/app/Http/Middleware/Cors.php
+++ streamline/app/Http/Middleware/Cors.php
@@ -6,19 +6,44 @@ use Closure;
 
 class Cors
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    $response = $next($request);
+    $IlluminateResponse = 'Illuminate\Http\Response';
+    $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+    $headers = [
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers' => 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers',
+    ];
+
+    if ($response instanceof $IlluminateResponse) {
+      foreach ($headers as $key => $value) {
+        $response->header($key, $value);
+      }
+      return $response;
+    }
+
+    if ($response instanceof $SymfonyResopnse) {
+      foreach ($headers as $key => $value) {
+        $response->headers->set($key, $value);
+      }
+      return $response;
+    }
+
+    return $response;
+    /*
       return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept' );
-    
-    }
+    */
+  }
 }
